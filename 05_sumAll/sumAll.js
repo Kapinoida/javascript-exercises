2092a4c258531145b55ae189c727ccb1e4649a11
David Plaskett <dcplaskett@gmail.com>
2022-01-12 14:42:18 -0600

    provided solution and removed skip

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < 0 || typeof args[i] != 'number') {
+      return 'ERROR'
+    }
+  }
 
+  let total = 0
+  const first = args[0]
+  const sec = args[1]
+  
+  if (first > sec) {
+    for (let i = sec; i < first + 1; i++) {
+      total += i
+    }
+  } else {
+    for (let i = first; i < sec + 1; i++) {
+      total += i
+    }
+  }
+  return total
 };
 
 // Do not edit below this line
