830595f6267b9868d2bdb7d18d0ede382a903423
David Plaskett <dcplaskett@gmail.com>
2022-01-18 21:11:28 -0600

    complete excercise and removed skip

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let stripped = string.split('').filter(x => x.match(/[a-z]|[0-9]/gi)).join('').toLowerCase();
+  let reverse = stripped.split('').reverse('').join('');
+  return stripped == reverse;
 };
 
 // Do not edit below this line
