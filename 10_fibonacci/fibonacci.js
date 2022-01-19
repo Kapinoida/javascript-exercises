f367b6f2e554943d7f31a83ab9814f87ea231b39
David Plaskett <dcplaskett@gmail.com>
2022-01-18 21:18:58 -0600

    completed and removed skip

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if(num < 0) return 'OOPS';
+  let a = 0;
+  let b = 1;
+  for (let i = 1; i < num; i++) {
+    let holder = b;
+    b = a + b;
+    a = holder;
+  }
+  return b;
 };
 
 // Do not edit below this line
