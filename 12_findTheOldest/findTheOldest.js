d5c4116b4122b912b95d661f68d1b2a556179fef
David Plaskett <dcplaskett@gmail.com>
2022-01-18 21:50:36 -0600

    complete and removed skip

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  
+  return arr.reduce((oldest, next) => {
+    const oldAge = age(oldest.yearOfBirth, oldest.yearOfDeath)
+    const nextAge = age(next.yearOfBirth, next.yearOfDeath)
+    return oldAge < nextAge ? next : oldest;
+  })
 };
+
+const currentYear = new Date().getFullYear();
+
+const age = function(born, dead) {
+  if(!dead) {
+    dead = currentYear;
+  }
+  return dead - born;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
